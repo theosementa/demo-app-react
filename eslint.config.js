071373b454276044422901f8a9573f4b3153d9cc
Theo Sementa <theosementa@gmail.com>
2025-07-25 14:08:16 +0200

    feat: update README.md

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
+import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
--- src/epic/calculator/calculator.view.tsx
+++ src/epic/calculator/calculator.view.tsx
@@ -1,17 +1,16 @@
-import { observer } from "mobx-react-lite";
-import { useMemo } from "react";
-import { Calculator } from "./calculator";
+import { observer } from 'mobx-react-lite';
+import { useMemo } from 'react';
+import { Calculator } from './calculator';
 
 export const CalculatorView = observer(() => {
-
   const calculator = useMemo(() => new Calculator(), []);
 
   const firstNumber = calculator.randomInt(1, 10);
   const secondNumber = calculator.randomInt(1, 10);
 
   return (
-    <div className='flex flex-col items-center justify-center gap-8'>
-      <p className='text-2xl'>{calculator.result}</p>
+    <div className="flex flex-col items-center justify-center gap-8">
+      <p className="text-2xl">{calculator.result}</p>
       <button
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
         onClick={() => calculator.add(firstNumber, secondNumber)}
@@ -20,5 +19,4 @@ export const CalculatorView = observer(() => {
       </button>
     </div>
   );
-
-})
+});
